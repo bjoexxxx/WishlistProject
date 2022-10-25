9a8985e9cebefa8cd952a93ef8d641eef63f8c0d
KristianWede <kris79s4@stud.kea.dk>
2022-10-25 09:57:00 +0200

    Added folders

---
+++ src/main/java/com/example/wishlistproject/controller/WishWebController.java
@@ -0,0 +1,17 @@
+package com.example.wishlistproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WishWebController {
+
+    @GetMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+
+
+
+}
