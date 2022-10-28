dfea1c252fa64337b0786539df41753b38bfbb98
Mark <mark771e@stud.kea.dk>
2022-10-28 11:08:30 +0200

    WishWebController.java : added - WishRepository attribute added - /showWishLists action

--- src/main/java/com/example/wishlistproject/controller/WishWebController.java
+++ src/main/java/com/example/wishlistproject/controller/WishWebController.java
@@ -1,15 +1,33 @@
 package com.example.wishlistproject.controller;
 
+import com.example.wishlistproject.repository.WishRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WishWebController {
 
+    private WishRepository wishRepository;
+
+
+    public WishWebController(WishRepository p) {
+        wishRepository = p;
+    }
+
     @GetMapping("/")
     public String showIndex(){
         return "html/index";
     }
+
+    @GetMapping("/showWishLists")
+    public String showWishLists () {
+
+        return "";
+
+    }
+
+
+
 
 
 }
