79e013370e6b3a22a39ab48d2c2cf4bb54fb7073
ferh0162 <ferh0162@stud.kea.dk>
2022-10-31 12:55:46 +0100

    rettet i index html

--- src/main/java/com/example/wishlistproject/controller/WishWebController.java
+++ src/main/java/com/example/wishlistproject/controller/WishWebController.java
@@ -53,6 +53,7 @@ public class WishWebController {
         @RequestParam("wishlist_name") String wishlistName,
         @RequestParam("user_first_name") String userFirstName,
         @RequestParam("user_last_name") String userLastName) {
+
         model.addAttribute("wishlist_name", wishlistName);
         model.addAttribute("user_first_name", userFirstName);
         model.addAttribute("user_last_name", userLastName);
@@ -68,7 +69,5 @@ public class WishWebController {
 
 
         return "redirect:/";
-
-
     }
 }
